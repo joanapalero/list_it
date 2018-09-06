d4f7853cc256a59e1bf7413a18e51e5f62718c8c
Joana P <joana.palero24@gmail.com>
2018-09-06 11:08:04 -0400

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+# Redirects user to sign up page
+  get '/signup' do
+    if logged_in?
+      redirect '/tasks'
+    else
+    erb :'users/signup'
+    end
+  end
+
+# Helps user sign into account based on username and password
+  post '/signup' do
+    if params[:username] == "" || params[:password] == ""
+      redirect '/signup'
+    else
+      @user = User.create(:username => params[:username], :password => params[:password])
+      session[:user_id] = @user.id
+    redirect '/tasks'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/tasks'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/tasks'
+    else
+      redirect '/login'
+    end
+  end
+
+# Logs user out
+  get '/logout' do
+    if session[:user_id] != nil
+      session.clear
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
+
+end
