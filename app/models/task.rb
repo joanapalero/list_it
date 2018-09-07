5680fbb3975b8c07212eefb56cf47b981e14bea0
Joana Palero <joana.palero24@gmail.com>
2018-09-07 19:58:07 -0400

    Removed and updated files.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,7 +2,3 @@ class Task < ActiveRecord::Base
   belongs_to :list
   belongs_to :user
 end
-
-def self.search(search)
-  where("task name LIKE ? OR task LIKE ? ", "%#{search}%", "%#{search}%") 
-end
