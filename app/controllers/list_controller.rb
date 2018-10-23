27f18d7f43876edc04b70b5ea4596c06d61ad4fe
Joana P <joana.palero24@gmail.com>
2018-10-23 19:07:37 -0400

    Updated list controller.

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,9 +1,55 @@
 class ListsController < ApplicationController
 
-  get '/lists/new' do
-    redirect_if_not_logged_in
-    erb :'/lists/new'
+  get '/lists' do
+      if logged_in?
+        @user = User.find(session[:user_id])
+        #show list based on user_id
+        @list = List.where(user_id: current_user)
+        # binding.pry
+        erb :"index"
+      else
+      # redirect "/redirect_if_not_logged_in"
+      #this will redirect user to another page if they try to access another list
+      redirect "/failure"
+    end
   end
+
+  # This will show the Lists
+    post '/lists' do
+      if logged_in?
+        @user = User.find_by(id: session[:user_id])
+      if params[:list].empty?
+        redirect "/lists/new"
+      else
+        @user = User.find_by(id: session[:user_id])
+        # this will create a new list if list is empty
+      @list = List.new
+      @list.user_id = @user_id
+      @list.save
+      redirect '/tasks'
+      end
+    else
+      redirect "/login"
+    end
+  end
+
+
+  # #Original code
+  # get '/lists/new' do
+  #   redirect_if_not_logged_in
+  #   erb :'/lists/new'
+  # end
+
+  # GET: /lists/new
+  get "/lists/new" do
+    if logged_in?
+      @user = User.find_by(id: session[:user_id])
+      erb :"/lists/new"
+    else
+      redirect "/failure"
+    end
+  end
+
 
   post '/lists/new' do
     @user = current_user
@@ -12,11 +58,24 @@ class ListsController < ApplicationController
     redirect '/tasks'
   end
 
-  get '/lists/:id/edit' do
-    redirect_if_not_logged_in
-    @list = List.find_by_id(params[:id])
-    erb :'lists/edit'
-  end
+#original code
+  # get '/lists/:id/edit' do
+  #   redirect_if_not_logged_in
+  #   @list = List.find_by_id(params[:id])
+  #   erb :'lists/edit'
+  # end
+
+  # GET: /lists/5
+      get "/lists/:id/edit" do
+        @user = User.find_by(id: session[:user_id])
+        @list = List.find(params[:id])
+        if @list && @list.user == current_user
+
+        erb :"/lists/edit"
+        else
+          redirect "/tasks"
+        end
+      end
 
   patch '/lists/:id' do
     @list = List.find_by_id(params[:id])
