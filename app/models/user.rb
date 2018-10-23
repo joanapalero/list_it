7ed22c30756b15b7df489c862337d074f850373e
Joana P <joana.palero24@gmail.com>
2018-10-23 19:08:30 -0400

    Updated to include validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :lists
   has_many :tasks, through: :lists
+  validates_uniqueness_of :username
+  validates :username, presence: true
+  validates :password, presence: true
 end
